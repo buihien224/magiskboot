cb3fc374b38ca07f6a14809a89a625f8ef7702fa
xiaoxindada <2245062854@qq.com>
2022-02-18 06:33:05 +0800

    Add patchs
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
 
+BUILD=true
 NDK_URL="https://dl.google.com/android/repository/android-ndk-r23b-linux.zip"
 ZIP_NAME="NDK.zip"
+
 setup_ndk() {
   wget "${NDK_URL}" -O "${ZIP_NAME}"
   7za x "${ZIP_NAME}"
@@ -15,5 +17,23 @@ build() {
   ndk-build
 }
 
-[ "$1" = "setup" ] && setup_ndk
-build && cd $LOCALDIR
+apply_patches() {
+  export BUILD=false
+  for p in patchs/* ;do
+    if ! git am -3 < $p ;then
+      patch -p1 < $p
+    fi
+  done
+
+}
+
+if echo $@ | grep "patch" ;then
+  apply_patches
+fi
+
+if [ $BUILD = true ];then
+  if echo $@ | grep -q setup; then
+    setup_ndk
+  fi
+  build && cd $LOCALDIR
+fi
